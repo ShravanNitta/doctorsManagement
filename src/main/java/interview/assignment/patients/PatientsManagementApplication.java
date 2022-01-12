2a72549f5269a80d465657b97716ac54797d27bb
shravan.nitta <shravan.nitta@tech5-sa.com>
2022-01-12 17:22:53 +0300

    initial commit

--- src/main/java/interview/assignment/patients/PatientsManagementApplication.java
+++ src/main/java/interview/assignment/patients/PatientsManagementApplication.java
@@ -1,8 +1,13 @@
 package interview.assignment.patients;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author Shravan Nitta
+ */
 
 @SpringBootApplication
 @ComponentScan(basePackages = "interview.assignment.patients.*")
